3b2de8c7de1abccc50565cf2df9767c90f08e166
ivad-passos <126341645+ivad-passos@users.noreply.github.com>
2023-10-31 17:36:28 -0300

    PROVA DE AED FIM

--- prova aed/q3.c
+++ prova aed/q3.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "LISTASE.H"
+
+#include "LISTADE.H"
         
     void seed(){
         srand(time(NULL));
@@ -10,44 +11,74 @@
     int dados(){
         int num;
         num = rand() % 6;
-        return num;
+        return num + 1;
     }
 
-    void jogo(tp_listase *caminho){
-        tp_listase *jogador;
+    int jogo(tp_listad *caminho){
+        tp_no *jogador;
         int jogadas = 0;
+        jogador = caminho->ini;
 
-        jogador = caminho;
+        while(jogador->info != 'f'){
 
-        while(jogador != NULL){
-            printf("\n%c", jogador->info);
-            if(jogador->info =='c') 
-                printf(" => estou em casa\n");
-            if(jogador->info =='0') 
-                printf(" => destruir\n");
-            if(jogador->info =='f') 
-                printf(" => terminei!!!\n");
-            else 
-                printf("\n");
-            jogador = jogador->prox;
+            printf("\n--\n %c", jogador->info); // printa a casa atual
+
+            switch (jogador->info)
+            {
+            case '1':
+                jogador = jogador->ant;
+                break;
+            case '2':
+                jogador = jogador->ant;
+                jogador = jogador->ant;
+                break;
+            case '0':
+                jogador = jogador->ant;
+                jogador = jogador->ant;
+                remove_listad(caminho,'0');
+                break;
+            
+            default:
+                break;
+            }
+
+            int dado = dados(); // anda
+            jogadas += 1;
+            for (int i = 0; i < dado; i++){
+                if(jogador->prox == NULL)
+                    break;
+                jogador = jogador->prox;
+            }
+            
         }
+        return jogadas;
     }
 
 int main(){
-    tp_listase *lista;
-        lista = inicializa_listase();
-        insere_listase_no_fim(&lista, 'c');
-        insere_listase_no_fim(&lista, '1');
-        insere_listase_no_fim(&lista, '2');
-        insere_listase_no_fim(&lista, '0');
-        insere_listase_no_fim(&lista, '1');
-        insere_listase_no_fim(&lista, 'c');
-        insere_listase_no_fim(&lista, '0');
-        insere_listase_no_fim(&lista, '1');
-        insere_listase_no_fim(&lista, 'f');
+    tp_listad *lista;
+    lista = inicializa_listad();
+    insere_listad_no_fim(lista, 'i');
+    insere_listad_no_fim(lista, '1');
+    insere_listad_no_fim(lista, 'c');
+    insere_listad_no_fim(lista, '0');
+    insere_listad_no_fim(lista, '1');
+    insere_listad_no_fim(lista, 'c');
+    insere_listad_no_fim(lista, '1');
+    insere_listad_no_fim(lista, '2');
+    insere_listad_no_fim(lista, 'f');
+
+    imprime_listad(lista,1);
+
+    printf("\n jogo vai comecar \n");
+    printf("-----------------------\n");
     
-    int num = dados();
     seed();
-    
-    jogo(lista);
+
+    int jogadas = 0;
+    jogadas = jogo(lista);
+    printf("\n\njogadas = %d", jogadas);
+
+    printf("\nlista final = ");
+
+    imprime_listad(lista,1);
 }
