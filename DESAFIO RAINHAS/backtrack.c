1bca539025cdd882b5dd0ef2850f73c285a41458
Davi Passos <126341645+ivad-passos@users.noreply.github.com>
2023-10-31 09:42:02 -0300

    Create backtrack.c

---
+++ DESAFIO RAINHAS/backtrack.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int caminho(int *matriz[7][7],int i, int j, int n){
+int soma;
+    if(soma == 10) return 1;
+    
+        for (int i = 0; i < 7; i++)
+            for (int j = 0; j < 7; j++)
+            {
+                
+            }
+            
+}
+
+int main(){
+    int matriz[7][7] = {
+                        {0,1,2,4,5,8,9},
+                        {1,1,7,8,2,5,9},
+                        {2,4,6,5,1,3,1},
+                        {3,2,1,4,3,9,8},
+                        {10,2,1,5,8,4,9},
+                        {8,9,5,4,2,1,10},
+                        {5,2,3,1,5,6,7}
+    };
+
+
+
+
+
+
+
+}
