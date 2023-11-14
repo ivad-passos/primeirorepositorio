77af455ab0145091c03effcc587f98815d7fa4a4
Davi Passos <126341645+ivad-passos@users.noreply.github.com>
2023-11-14 12:17:35 -0300

    update

--- ARVORES/AULA1/AULA1.C
+++ ARVORES/AULA1/AULA1.C
@@ -4,7 +4,9 @@
 int main() {
   tp_arvore arvore;
   arvore = inicializa_arvore();
-  if (arvore_vazia(arvore) == 1) {
-    printf("vazia");
-  }
+  insere_no(&arvore,6);
+  insere_no(&arvore,3);
+  insere_no(&arvore,9);
+  printf("%d", arvore->)
+  
 }
